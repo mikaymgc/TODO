139a4e7d5bfbbb2c569bf1b5ff07ebc29371d0d0
mikaymgc <mk25f.y@gmail.com>
2022-01-25 14:07:32 +0900

    フォームの値を取得

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-//コメント
-console.log("hello");
+const form = document.getElementById("form");
+const input = document.getElementById("input")
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  console.log(input.value);
+});
